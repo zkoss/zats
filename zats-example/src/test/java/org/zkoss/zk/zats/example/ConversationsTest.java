2b061a97ca6aa4aa7e8737cffd862bc83bea47d5
paowang@potix.com <paowang@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-06 09:21:05 +0000

    git-svn-id: https://zkats.googlecode.com/svn/trunk@27 d67f8428-efe3-eb30-9e60-a2a0d17496d8

---
+++ zats-example/src/test/java/org/zkoss/zk/zats/example/ConversationsTest.java
@@ -0,0 +1,53 @@
+package org.zkoss.zk.zats.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import javax.servlet.http.HttpSession;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zats.core.Conversations;
+import org.zkoss.zats.core.Searcher;
+import org.zkoss.zats.core.component.DesktopNode;
+import org.zkoss.zats.core.component.operation.Clickable;
+import org.zkoss.zul.Label;
+
+public class ConversationsTest
+{
+	@BeforeClass
+	public static void init()
+	{
+		Conversations.start("./src/main/webapp"); // user can load by configuration file
+	}
+
+	@AfterClass
+	public static void end()
+	{
+		Conversations.stop();
+	}
+
+	@Test
+	public void test()
+	{
+		Conversations.open("/test.zul");
+
+		assertNotNull(Conversations.getSession());
+		assertNotNull(Conversations.getDesktop());
+
+		HttpSession session = Conversations.getSession();
+		DesktopNode desktop = Conversations.getDesktop();
+		assertEquals("session", session.getAttribute("msg"));
+		assertEquals("desktop", desktop.getAttribute("msg"));
+		assertEquals("hello", Searcher.find(desktop, "$msg").cast(Label.class).getValue());
+
+		for(int i = 0; i < 10; ++i)
+		{
+			Searcher.find(desktop, "$btn").as(Clickable.class).click();
+			assertEquals("s" + i, session.getAttribute("msg"));
+			assertEquals("d" + i, desktop.getAttribute("msg"));
+			assertEquals("" + i, Searcher.find(desktop, "$msg").cast(Label.class).getValue());
+		}
+
+		Conversations.clean();
+	}
+}
