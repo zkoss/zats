9928740fba3499c21ae69b6cd73995136c31f844
paowang@potix.com <paowang@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-08 03:08:05 +0000

    git-svn-id: https://zkats.googlecode.com/svn/trunk@30 d67f8428-efe3-eb30-9e60-a2a0d17496d8

--- zats-core/src/main/java/org/zkoss/zats/core/component/operation/OperationHandler.java
+++ zats-core/src/main/java/org/zkoss/zats/core/component/operation/OperationHandler.java
@@ -1,8 +1,11 @@
 package org.zkoss.zats.core.component.operation;
 
+import org.zkoss.zats.core.Conversation;
+import org.zkoss.zats.core.component.ComponentNode;
 
-public interface OperationHandler
-{
-	// void setOutput(Queue<JSON> commandQueue); 
-	<T extends Operation> T as(Class<T> c);
+public interface OperationHandler {
+
+	Class<Operation>[] getSupportedOperations();
+
+	<T extends Operation> T getOperation(Class<T> c, Conversation conversation, ComponentNode target);
 }
--- zats-core/src/main/java/org/zkoss/zats/core/component/operation/impl/GenericClickableHandler.java
+++ zats-core/src/main/java/org/zkoss/zats/core/component/operation/impl/GenericClickableHandler.java
@@ -1,20 +1,35 @@
 package org.zkoss.zats.core.component.operation.impl;
 
+import org.zkoss.zats.core.Conversation;
+import org.zkoss.zats.core.component.ComponentNode;
 import org.zkoss.zats.core.component.operation.Clickable;
 import org.zkoss.zats.core.component.operation.Operation;
 import org.zkoss.zats.core.component.operation.OperationHandler;
 
-public class GenericClickableHandler implements OperationHandler, Clickable
-{
+public class GenericClickableHandler implements OperationHandler, Clickable {
 
-	public Clickable click()
-	{
+	/**
+	 * @see org.zkoss.zats.core.component.operation.Clickable#click()
+	 */
+	public Clickable click() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public <T extends Operation> T as(Class<T> c)
-	{
-		return (T)this;
+	/**
+	 * @see org.zkoss.zats.core.component.operation.OperationHandler#getSupportedOperations()
+	 */
+
+	public Class<Operation>[] getSupportedOperations() {
+		return null;
+	}
+
+	/**
+	 * @see org.zkoss.zats.core.component.operation.OperationHandler#getOperation(java.lang.Class,
+	 *      org.zkoss.zats.core.Conversation, org.zkoss.zats.core.component.ComponentNode)
+	 */
+
+	public <T extends Operation> T getOperation(Class<T> c, Conversation conversation, ComponentNode target) {
+		return (T) this;
 	}
 }
