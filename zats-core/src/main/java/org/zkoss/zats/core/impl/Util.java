3e29bf0601d0f06b70d6a511dbf7706cd994234c
paowang@potix.com <paowang@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-13 04:22:25 +0000

    add utilities for zats core.
    git-svn-id: https://zkats.googlecode.com/svn/trunk@44 d67f8428-efe3-eb30-9e60-a2a0d17496d8

---
+++ zats-core/src/main/java/org/zkoss/zats/core/impl/Util.java
@@ -0,0 +1,58 @@
+package org.zkoss.zats.core.impl;
+
+import java.io.Closeable;
+import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utilities for implementation.
+ * @author pao
+ */
+public class Util
+{
+	/**
+	 * close resource without any exception.
+	 * @param r resource. If null, do nothing.
+	 */
+	public static void close(Closeable r)
+	{
+		try
+		{
+			r.close();
+		}
+		catch(Throwable e)
+		{
+		}
+	}
+
+	/**
+	 * parse zk version to uniform number for compare.
+	 * This method doesn't recognize beta or release candidate version.
+	 * @param version zk version string.
+	 * @return uniform number or null if argument is illegal.
+	 */
+	public static BigInteger parseVersion(final String version)
+	{
+		if(version == null || version.length() <= 0)
+			return null;
+		// parse string into byte array
+		Matcher m = Pattern.compile("\\s*(\\d+)\\.(\\d+)\\.(\\d+)\\.?(\\d*)\\s*").matcher(version);
+		if(!m.matches())
+			return null;
+		byte[] raw = new byte[4];
+		for(int i = 0; i < m.groupCount(); ++i)
+			raw[i] = m.group(i + 1).length() > 0 ? Byte.parseByte(m.group(i + 1)) : 0;
+		// create extended BigInteger
+		// Because raw data is big-endian, major number is most significant.
+		return new BigInteger(raw)
+		{
+			private static final long serialVersionUID = 2712388085709130462L;
+
+			public String toString()
+			{
+				return version;
+			}
+		};
+	}
+}
---
+++ zats-core/src/test/java/org/zkoss/zats/core/impl/UtilTest.java
@@ -0,0 +1,44 @@
+package org.zkoss.zats.core.impl;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+
+public class UtilTest
+{
+
+	@Test
+	public void testClose()
+	{
+		Util.close(null);
+		Util.close(new ByteArrayOutputStream());
+	}
+
+	@Test
+	public void testParseVersion()
+	{
+		String[] versions = new String[]{ "5.0.0", "5.0.1", "5.0.1.1", "5.0.2", "5.0.2.1", "6.0.0", "5.0.10", "5.0.9", "3.6.3", "3.6.2", "3.6.4", "5.0.8", "5.0.7.1", "5.0.7", "5.0.6", "5.0.5", "5.0.4", "5.0.3" };
+		String[] correct = new String[]{ "3.6.2", "3.6.3", "3.6.4", "5.0.0", "5.0.1", "5.0.1.1", "5.0.2", "5.0.2.1", "5.0.3", "5.0.4", "5.0.5", "5.0.6", "5.0.7", "5.0.7.1", "5.0.8", "5.0.9", "5.0.10", "6.0.0" };
+		List<BigInteger> list = new ArrayList<BigInteger>();
+		for(String version : versions)
+			list.add(Util.parseVersion(version));
+		Collections.sort(list);
+		System.out.println(list);
+		for(int i = 0; i < versions.length; ++i)
+			versions[i] = list.get(i).toString();
+		assertArrayEquals(correct, versions);
+
+		assertEquals(0, Util.parseVersion(" 6.0.0  ").compareTo(Util.parseVersion("\t6.0.0  ")));
+		assertEquals(1, Util.parseVersion(" 6.0.0").compareTo(Util.parseVersion("  5.1.1 ")));
+		assertNull(Util.parseVersion("dfasdflj"));
+		assertNull(Util.parseVersion(null));
+		assertNull(Util.parseVersion(""));
+		assertNull(Util.parseVersion(" "));
+	}
+}
