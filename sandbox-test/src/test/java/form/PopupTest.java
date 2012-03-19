3fa94a641ff8acd4e44d7c03190cb4c6a62224d2
hawkchen@potix.com <hawkchen@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-19 09:21:51 +0000

    test menu case.
    git-svn-id: https://zkats.googlecode.com/svn/trunk@75 d67f8428-efe3-eb30-9e60-a2a0d17496d8

---
+++ sandbox-test/src/test/java/form/PopupTest.java
@@ -0,0 +1,28 @@
+package form;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.zkoss.zats.mimic.Searcher.find;
+
+import org.junit.Test;
+import org.zkoss.zats.mimic.Conversations;
+import org.zkoss.zats.mimic.Searcher;
+import org.zkoss.zats.mimic.node.ComponentNode;
+import org.zkoss.zats.mimic.operation.Clickable;
+import org.zkoss.zul.Textbox;
+
+
+public class PopupTest extends TestCaseBase {
+
+	@Test
+	public void testClickMenu(){
+		Conversations.open("/form/popup.zul");
+		Textbox intro = Searcher.find("textbox[width='250px'").cast(Textbox.class);
+		assertNotNull(intro.getValue());
+		
+		ComponentNode menupopup = find("menupopup");
+		//clear text box text and verify
+		menupopup.getChild(0).as(Clickable.class).click();
+		assertEquals(0, intro.getValue().length());
+	}
+}
