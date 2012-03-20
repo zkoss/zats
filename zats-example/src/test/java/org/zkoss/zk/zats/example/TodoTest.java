077c5b5582a891af413ee62ef5800440746b9db1
hawkchen@potix.com <hawkchen@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-20 08:35:43 +0000

    add verification.
    git-svn-id: https://zkats.googlecode.com/svn/trunk@98 d67f8428-efe3-eb30-9e60-a2a0d17496d8

--- zats-example/src/test/java/org/zkoss/zk/zats/example/TodoTest.java
+++ zats-example/src/test/java/org/zkoss/zk/zats/example/TodoTest.java
@@ -1,6 +1,11 @@
 package org.zkoss.zk.zats.example;
 
 import static org.zkoss.zats.mimic.Searcher.find;
+import static org.junit.Assert.*;
+
+
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -8,6 +13,7 @@ import org.junit.Test;
 import org.zkoss.zats.mimic.Conversations;
 import org.zkoss.zats.mimic.operation.Clickable;
 import org.zkoss.zats.mimic.operation.Typeable;
+import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
 
 public class TodoTest {
@@ -35,11 +41,14 @@ public class TodoTest {
 //		find("textbox").as(Textbox.class).setValue("abc"); cause IllegalStateException: Components can be accessed only in event listeners
 		find("intbox").as(Typeable.class).type("3");
 		find("datebox").as(Typeable.class).type("2012/03/16");
-		find("button[label = 'Add'] ").as(Clickable.class).click();
+		find("button[label='Add'] ").as(Clickable.class).click();
 		
 		//verify each listcell's label
-		Listitem listitem = find("listbox").getChild(1).as(Listitem.class);
-		
+//		Listitem listitem = find("listbox").getChild(1).as(Listitem.class);
+		List cells = find("listbox").getChild(1).as(Listitem.class).getChildren();
+		assertEquals("one-item",((Listcell)cells.get(0)).getLabel());
+		assertEquals("3",((Listcell)cells.get(1)).getLabel());
+		assertEquals("2012/03/16",((Listcell)cells.get(2)).getLabel());
 		
 		
 		//update
