fc2039059441f744e8df3609e271d86981adfee1
hawkchen@potix.com <hawkchen@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-21 07:46:55 +0000

    * check type before casting. * log warning for select a null item.
    git-svn-id: https://zkats.googlecode.com/svn/trunk@113 d67f8428-efe3-eb30-9e60-a2a0d17496d8

--- zats-mimic/src/main/java/org/zkoss/zats/mimic/impl/operation/ListboxSelectAgentBuilder.java
+++ zats-mimic/src/main/java/org/zkoss/zats/mimic/impl/operation/ListboxSelectAgentBuilder.java
@@ -11,11 +11,17 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
  */
 package org.zkoss.zats.mimic.impl.operation;
 
+import java.util.logging.Logger;
+
 import org.zkoss.zats.mimic.ComponentAgent;
+import org.zkoss.zats.mimic.ConversationException;
 import org.zkoss.zats.mimic.operation.SelectAgent;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
 
 public class ListboxSelectAgentBuilder implements OperationAgentBuilder<SelectAgent> {
+	private static Logger logger = Logger.getLogger(ListboxSelectAgentBuilder.class.getName());
+	
 	public SelectAgent getOperation(final ComponentAgent target) {
 		return new SelectAgentImpl(target);
 	}
@@ -25,9 +31,16 @@ public class ListboxSelectAgentBuilder implements OperationAgentBuilder<SelectAg
 		}
 
 		public void select(int index) {
-			// TODO check type of the target
-			AuUtility.postOnSelect(target, target.as(Listbox.class)
-					.getItemAtIndex(index).getUuid());
+			if (target.is(Listbox.class)){
+				Listitem selected = target.as(Listbox.class).getItemAtIndex(index);
+				if (selected==null){
+					AuUtility.postOnSelect(target, selected.getUuid());
+				}else{
+					logger.warning("select index:"+index+" is null.");
+				}
+			}else{
+				throw new ConversationException(target+" can't select");
+			}
 		}
 	}
 }
