cf0e5dfe1637670e243573b37729df70bbd05b82
atticcat@gmail.com <atticcat@gmail.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-23 03:22:58 +0000

    ext6 initiator
    git-svn-id: https://zkats.googlecode.com/svn/trunk@128 d67f8428-efe3-eb30-9e60-a2a0d17496d8

---
+++ zats-mimic-ext6/src/main/java/org/zkoss/zats/mimic/impl/Ext6Initiator.java
@@ -0,0 +1,72 @@
+/* Initial.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		2012/3/23 Created by dennis
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.zats.mimic.impl;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+import org.zkoss.zats.mimic.ConversationException;
+import org.zkoss.zats.mimic.impl.au.EventDataBuilder;
+import org.zkoss.zats.mimic.impl.au.EventDataManager;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.util.WebAppInit;
+import org.zkoss.zul.event.RenderEvent;
+
+/**
+ * @author dennis
+ *
+ */
+public class Ext6Initiator implements WebAppInit{
+
+	public void init(WebApp wapp) throws Exception {
+		
+		//testcases and mimic server is in the same vm. 
+		//so it is ok to register builder by webapp init
+//		EventDataManager.registerBuilder("6.0.0","*", RenderEvent.class, new EventDataBuilder(){
+//			public Map<String, Object> build(Event event,Map<String,Object> data) {
+//				RenderEvent evt = (RenderEvent)event;
+//				setEssential(data,"items",evt.getItems());//id set of items
+//				return data;
+//			}});
+	}
+//	
+//	@SuppressWarnings("unchecked")
+//	static private Object toSafeJsonObject(Object obj){
+//		if(obj instanceof Set){
+//			//exception if data is Set
+//			//>>Unexpected character (n) at position 10.
+//			//>>	at org.zkoss.json.parser.Yylex.yylex(Yylex.java:610)
+//			//>>	at org.zkoss.json.parser.JSONParser.nextToken(JSONParser.java:270)
+//			return new ArrayList((Set)obj);
+//		}
+//		return obj;
+//	}
+//	
+//	static private void setEssential(Map<String,Object> data,String key, Object obj){
+//		if(obj==null) throw new ConversationException("data of "+key+" is null");
+//		data.put(key, toSafeJsonObject(obj));
+//	}
+//	
+//	static private void setOptional(Map<String,Object> data,String key, Object obj){
+//		if(obj==null) return;
+//		data.put(key, toSafeJsonObject(obj));
+//	}
+//
+//	static private void setReference(Map<String,Object> data,Component comp){
+//		if(comp==null) return;
+//		data.put("reference", comp.getUuid());
+//	}
+
+}
