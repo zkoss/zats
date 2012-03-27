5c9e8e8b5ff99150bccc943352c6575e518d03ed
paowang@potix.com <paowang@potix.com@d67f8428-efe3-eb30-9e60-a2a0d17496d8>
2012-03-27 08:06:50 +0000

    add a test case
    git-svn-id: https://zkats.googlecode.com/svn/trunk@171 d67f8428-efe3-eb30-9e60-a2a0d17496d8

---
+++ zats-mimic-ext6/src/test/java/org/zkoss/zats/testcase/BasicAgentTest.java
@@ -0,0 +1,73 @@
+/* BasicAgentTest.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Mar 27, 2012 Created by pao
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+ */
+package org.zkoss.zats.testcase;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zats.mimic.Conversations;
+import org.zkoss.zats.mimic.Searcher;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Toolbarbutton;
+
+/**
+ * @author pao
+ * 
+ */
+public class BasicAgentTest {
+	@BeforeClass
+	public static void init() {
+		Conversations.start(".");
+	}
+
+	@AfterClass
+	public static void end() {
+		Conversations.stop();
+	}
+
+	@After
+	public void after() {
+		Conversations.clean();
+	}
+
+	@Test
+	public void testToolbarButtonCheck() {
+		Conversations.open("/~./basic/toolbar.zul");
+
+		for (int i = 1; i <= 6; ++i)
+			assertEquals("tbb" + i, Searcher.find("#tbb" + i).as(Toolbarbutton.class).getLabel());
+
+		Label clicked = Searcher.find("#clicked").as(Label.class);
+		assertEquals("", clicked.getValue());
+
+		for (int i = 1; i <= 6; ++i) {
+			Searcher.find("#tbb" + i).click();
+			assertEquals("tbb" + i, clicked.getValue());
+		}
+
+		Label checked = Searcher.find("#checked").as(Label.class);
+		assertEquals("", checked.getValue());
+
+		String[] values = { "tbb4 ", "tbb4 tbb5 ", "tbb4 tbb5 tbb6 ", "tbb4 tbb6 " };
+		for (int i = 4; i <= 6; ++i) {
+			Searcher.find("#tbb" + i).check(true);
+			assertEquals(values[i - 4], checked.getValue());
+			assertEquals("tbb6", clicked.getValue()); // "check" should not perform "click"
+		}
+
+		Searcher.find("#tbb5").check(false);
+		assertEquals(values[3], checked.getValue());
+	}
+}
