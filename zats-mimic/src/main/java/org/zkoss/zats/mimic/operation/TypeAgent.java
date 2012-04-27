68aee745c028ca0b33bcae57fa598c91b705cf9c
paowang <paowang@potix.com>
2012-04-27 17:07:59 +0800

    add typing operation

--- zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/TypeAgent.java
+++ zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/TypeAgent.java
@@ -12,8 +12,7 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 package org.zkoss.zats.mimic.operation;
 
 /**
- * The typing operation.
- * 
+ * The type operation.
  * @author pao
  */
 public interface TypeAgent extends OperationAgent {
@@ -22,9 +21,7 @@ public interface TypeAgent extends OperationAgent {
 	 * To type data into a component. The value should be valid for target
 	 * component. e.g. Intbox only accepts integer. If the target is Datebox or Timebox,
 	 * the format depends on its "format" attribute. 
-	 * 
-	 * @param value
-	 *            the input value
+	 * @param value the input value
 	 */
 	public void type(String value);
 }
