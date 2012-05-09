1b37a0a8962f5a4b67dd6df94c08a7ca73fa11a0
Dennis Chen <atticcat@gmail.com>
2012-05-09 09:24:18 +0800

    javadoc

--- zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/BookmarkAgent.java
+++ zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/BookmarkAgent.java
@@ -11,12 +11,17 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zats.mimic.operation;
 
+import org.zkoss.zk.ui.event.Events;
+
 /**
- * To change the bookmark of a desktop
+ * To change the bookmark of a desktop.
  * @author dennis
- *
  */
 public interface BookmarkAgent extends OperationAgent {
 	
+	/**
+	 * change bookmark of a desktop.
+	 * @param value
+	 */
 	void bookmark(String value);
 }
