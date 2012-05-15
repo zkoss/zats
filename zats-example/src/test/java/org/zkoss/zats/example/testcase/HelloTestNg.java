a2bde2387c0d8beb6705a75a7d7bee158325d128
Hawk Chen <hawkchen@potix.com>
2012-05-15 11:19:14 +0800

    add TestNG example

---
+++ zats-example/src/test/java/org/zkoss/zats/example/testcase/HelloTestNg.java
@@ -0,0 +1,43 @@
+package org.zkoss.zats.example.testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.zkoss.zats.mimic.ComponentAgent;
+import org.zkoss.zats.mimic.DesktopAgent;
+import org.zkoss.zats.mimic.Zats;
+import org.zkoss.zul.Label;
+
+
+
+
+public class HelloTestNg {
+	@BeforeClass
+	public static void init() {
+		Zats.init("./src/main/webapp"); 
+	}
+
+	@AfterClass
+	public static void end() {
+		Zats.end();
+	}
+
+	@AfterMethod
+	public void after() {
+		Zats.cleanup();
+	}
+
+	@Test
+	public void test() {
+		DesktopAgent desktop = Zats.newClient().connect("/hello.zul");
+
+		ComponentAgent button = desktop.query("button");
+		ComponentAgent label = desktop.query("label");
+		
+		//button.as(ClickAgent.class).click();
+		button.click();
+		Assert.assertEquals("Hello Mimic", label.as(Label.class).getValue());
+	}
+}
