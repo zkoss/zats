b7e80a047c2a71bbe3728d4c8aae6f1386c4d036
paowang <paowang@potix.com>
2012-06-29 18:14:27 +0800

    fine turn the comments.

--- zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/UploadAgent.java
+++ zats-mimic/src/main/java/org/zkoss/zats/mimic/operation/UploadAgent.java
@@ -18,13 +18,15 @@ import java.io.InputStream;
  * The agent for files uploading operation.
  * Supported components with <b>upload</b> property such as Button, Menuitem and Toolbarbutton.
  * DesktopAgent can be UploadAgent for specific case such as uploading by Fileupload.get().
+ * <b>Notice that you should use the same instance in one uploading iteration.</b>
  * @author pao
  * @since 1.1.0
  */
 public interface UploadAgent extends OperationAgent {
 
 	/**
-	 * Upload a file to ZK web application.
+	 * Upload a file from input stream to ZK web application.
+	 * The input stream won't be closed, it should be closed manually.
 	 * @param fileName specify name for uploading file, should not be null.
 	 * @param content content of uploaded file, should not be null.
 	 * @param contentType specify type of content. If null, indicate it is binary form.
