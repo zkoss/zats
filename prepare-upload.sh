36e0c07a606933dc2a55614c6e1ff52d6be8884d
Robert Wenzel <robertwenzel@potix.com>
2017-09-12 18:52:22 +0800

    automate release script

---
+++ prepare-upload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+VERSION=$1
+
+rm -rf ./target/to_upload
+
+UPLOAD_FOLDER=./target/to_upload/$VERSION
+BIN_FOLDER=$UPLOAD_FOLDER/bin
+MVN_FOLDER=$UPLOAD_FOLDER/maven
+
+mkdir -p $UPLOAD_FOLDER
+mkdir -p $BIN_FOLDER
+mkdir -p $MVN_FOLDER
+
+cp zats/target/zats-$VERSION-javadoc.jar $UPLOAD_FOLDER
+
+cp zats/target/zats-mimic-$VERSION.zip $BIN_FOLDER
+cp zats/target/zats-$VERSION-javadoc.jar $BIN_FOLDER
+
+cp zats-common/target/zats-common-$VERSION-bundle.jar $MVN_FOLDER
+cp zats-mimic/target/zats-mimic-$VERSION-bundle.jar $MVN_FOLDER
+cp zats-mimic-ext6/target/zats-mimic-ext6-$VERSION-bundle.jar $MVN_FOLDER
+cp zats-mimic-ext7/target/zats-mimic-ext7-$VERSION-bundle.jar $MVN_FOLDER
