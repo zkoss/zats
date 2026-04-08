44e7b5ef57d8cacc59e91bd83b2de51a4fcce634
James Chu <marchuqq@gmail.com>
2026-04-08 19:13:55 +0800

    seperate upload files path

--- prepare-upload.sh
+++ prepare-upload.sh
@@ -4,17 +4,27 @@ VERSION=$1
 rm -rf ./target/to_upload
 
 UPLOAD_FOLDER=./target/to_upload/$VERSION
-BIN_FOLDER=$UPLOAD_FOLDER/bin
-MVN_FOLDER=$UPLOAD_FOLDER/maven
-
 mkdir -p $UPLOAD_FOLDER
-mkdir -p $BIN_FOLDER
-mkdir -p $MVN_FOLDER
 
-cp zats/target/zats-$VERSION-javadoc.jar $UPLOAD_FOLDER/zats-javadoc-$VERSION.zip
 
-cp zats/target/zats-mimic-$VERSION.zip $BIN_FOLDER
+MIMIC_BIN_FOLDER=$UPLOAD_FOLDER/zats-mimic/release/$VERSION/bin
+mkdir -p $MIMIC_BIN_FOLDER
 
-cp zats-common/target/zats-common-$VERSION-bundle.jar $MVN_FOLDER
-cp zats-mimic/target/zats-mimic-$VERSION-bundle.jar $MVN_FOLDER
-cp zats-mimic-ext/target/zats-mimic-ext-$VERSION-bundle.jar $MVN_FOLDER
+cp zats/target/zats-$VERSION-javadoc.jar $UPLOAD_FOLDER/zats/release/$VERSION/zats-javadoc-$VERSION.zip
+
+cp zats/target/zats-mimic-$VERSION.zip $MIMIC_BIN_FOLDER
+
+COMMON_MVN_FOLDER=$UPLOAD_FOLDER/zats-common/release/maven
+mkdir -p $COMMON_MVN_FOLDER
+
+cp zats-common/target/zats-common-$VERSION-bundle.jar $COMMON_MVN_FOLDER
+
+MIMIC_MVN_FOLDER=$UPLOAD_FOLDER/zats-mimic/release/maven
+mkdir -p $MIMIC_MVN_FOLDER
+
+cp zats-mimic/target/zats-mimic-$VERSION-bundle.jar $MIMIC_MVN_FOLDER
+
+MIMIC_EXT_MVN_FOLDER=$UPLOAD_FOLDER/zats-mimic-ext/release/maven
+mkdir -p $MIMIC_EXT_MVN_FOLDER
+
+cp zats-mimic-ext/target/zats-mimic-ext-$VERSION-bundle.jar $MIMIC_EXT_MVN_FOLDER
